53f2a8ccdd64cf44e22806a922e9e403aad4a680
Rina <37088203+SirRina@users.noreply.github.com>
2022-04-05 19:33:03 -0300

    Performance.

--- EKG_element_ui.cpp
+++ EKG_element_ui.cpp
@@ -48,182 +48,190 @@ void EKG_Frame::OnPreEvent(SDL_Event Event) {
 void EKG_Frame::OnEvent(SDL_Event Event) {
     EKG_AbstractElement::OnEvent(Event);
 
-    if (Event.type == SDL_FINGERMOTION) {
-        float FX = Event.tfinger.x;
-        float FY = Event.tfinger.y;
+    switch (Event.type) {
+        case SDL_FINGERMOTION: {
+            float FX = Event.tfinger.x;
+            float FY = Event.tfinger.y;
 
-        EKG::ScaledFingerPos(FX, FY);
-        bool MovingEvent;
+            EKG::ScaledFingerPos(FX, FY);
+            bool MovingEvent;
 
-        if (this->Dragging) {
-            float X = FX - this->DragX;
-            float Y = FY - this->DragY;
-            float W = this->Rect.W;
-            float H = this->Rect.H;
+            if (this->Dragging) {
+                float X = FX - this->DragX;
+                float Y = FY - this->DragY;
+                float W = this->Rect.W;
+                float H = this->Rect.H;
 
-            if (this->GetMasterId() != 0) {
-                if (!this->FreeDragAndDrop) {
-                    if (X < 0) {
-                        X = 0;
-                    }
+                if (this->GetMasterId() != 0) {
+                    if (!this->FreeDragAndDrop) {
+                        if (X < 0) {
+                            X = 0;
+                        }
 
-                    if (Y < 0) {
-                        Y = 0;
-                    }
+                        if (Y < 0) {
+                            Y = 0;
+                        }
 
-                    if (X + W > this->ScaledWidth) {
-                        X = this->ScaledWidth - W;
-                    }
+                        if (X + W > this->ScaledWidth) {
+                            X = this->ScaledWidth - W;
+                        }
 
-                    if (Y + H > this->ScaledHeight) {
-                        Y = this->ScaledHeight - H;
-                    }
-                }
-
-                this->SyncX = X;
-                this->SyncY = Y;
-
-                this->SyncPos();
-            } else {
-                this->Rect.X = FX - this->DragX;
-                this->Rect.Y = FY - this->DragY;
-            }
-
-            MovingEvent = true;
-        }
-
-        if (this->Resizing != 0) {
-            float X;
-            float Y;
-            float W;
-            float H;
-
-            if (this->Resizing == EKG::Dock::LEFT || this->Resizing == EKG::Dock::TOP) {
-                float DragUpdateX = FX - this->DragW;
-                float DragUpdateY = FY - this->DragH;
-
-                float PredictionW = this->PreviousW + (this->PreviousX - DragUpdateX);
-                float PredictionH = this->PreviousH + (this->PreviousY - DragUpdateY);
-
-                X = PredictionW < (float) this->MinimumWidth ? this->PreviousX + this->PreviousW - (float) this->MinimumWidth : DragUpdateX;
-                Y = PredictionH < (float) this->MinimumHeight ? this->PreviousY + this->PreviousH - (float) this->MinimumHeight : DragUpdateY;
-
-                if (this->GetMasterId() == 0) {
-                    this->Rect.X = X;
-                    this->Rect.Y = Y;
-                } else {
-                    X -= this->ScaledX;
-                    Y -= this->ScaledY;
-
-                    if (X < 0) {
-                        X = 0;
-                    }
-
-                    if (Y < 0) {
-                        Y = 0;
+                        if (Y + H > this->ScaledHeight) {
+                            Y = this->ScaledHeight - H;
+                        }
                     }
 
                     this->SyncX = X;
                     this->SyncY = Y;
 
                     this->SyncPos();
+                } else {
+                    this->Rect.X = FX - this->DragX;
+                    this->Rect.Y = FY - this->DragY;
                 }
 
-                // Diff Width & Height. Not prediction anymore, recycled.
-                PredictionW = this->PreviousW + (this->PreviousX - this->Rect.X);
-                PredictionH = this->PreviousH + (this->PreviousY - this->Rect.Y);
-
-                W = PredictionW < (float) this->MinimumWidth ? (float) this->MinimumWidth : PredictionW;
-                H = PredictionH < (float) this->MinimumHeight ? (float) this->MinimumHeight : PredictionH;
-
-                this->Rect.W = W;
-                this->Rect.H = H;
-            } else {
-                X = this->Rect.X;
-                Y = this->Rect.Y;
-
-                W = FX - this->DragW;
-                H = FY - this->DragH;
-
-                if (this->GetMasterId() != 0) {
-                    if (X + W > this->ScaledX + this->ScaledWidth) {
-                        W = this->ScaledX + this->ScaledWidth - X;
-                    }
-
-                    if (Y + H > this->ScaledY + this->ScaledHeight) {
-                        H = this->ScaledY + this->ScaledHeight - Y;
-                    }
-
-                    this->SyncPos();
-                }
-
-                this->Rect.W = W < (float) this->MinimumWidth ? (float) this->MinimumWidth : W;
-                this->Rect.H = H < (float) this->MinimumHeight ? (float) this->MinimumHeight : H;
+                MovingEvent = true;
             }
 
-            MovingEvent = true;
-        }
+            if (this->Resizing != 0) {
+                float X;
+                float Y;
+                float W;
+                float H;
 
-        if (MovingEvent && this->IsMaster()) {
-            this->SyncSize();
-            EKG_CORE->Sync(this->Rect.X, this->Rect.Y, this->Rect.W, this->Rect.H, this->Children);
-        }
-    }
+                if (this->Resizing == EKG::Dock::LEFT || this->Resizing == EKG::Dock::TOP) {
+                    float DragUpdateX = FX - this->DragW;
+                    float DragUpdateY = FY - this->DragH;
 
-    if (Event.type == SDL_FINGERDOWN) {
-        float FX = Event.tfinger.x;
-        float FY = Event.tfinger.y;
+                    float PredictionW = this->PreviousW + (this->PreviousX - DragUpdateX);
+                    float PredictionH = this->PreviousH + (this->PreviousY - DragUpdateY);
 
-        EKG::ScaledFingerPos(FX, FY);
+                    X = PredictionW < (float) this->MinimumWidth ? this->PreviousX + this->PreviousW - (float) this->MinimumWidth : DragUpdateX;
+                    Y = PredictionH < (float) this->MinimumHeight ? this->PreviousY + this->PreviousH - (float) this->MinimumHeight : DragUpdateY;
 
-        if (this->Hovered) {
-            if (this->DraggableDockFlags != 0 && !this->Dragging && this->Resizing == 0) {
-                int CollidingDock = EKG::PointCollideDock(this->DraggableDockFlags, FX, FY, 0, this->DragOffset, this->Rect);
-
-                if (CollidingDock != -1) {
                     if (this->GetMasterId() == 0) {
-                        this->DragX = FX - this->Rect.X;
-                        this->DragY = FY - this->Rect.Y;
+                        this->Rect.X = X;
+                        this->Rect.Y = Y;
                     } else {
-                        this->DragX = FX - (this->Rect.X - this->ScaledX);
-                        this->DragY = FY - (this->Rect.Y - this->ScaledY);
+                        X -= this->ScaledX;
+                        Y -= this->ScaledY;
+
+                        if (X < 0) {
+                            X = 0;
+                        }
+
+                        if (Y < 0) {
+                            Y = 0;
+                        }
+
+                        this->SyncX = X;
+                        this->SyncY = Y;
+
+                        this->SyncPos();
                     }
 
-                    // Say true for dragging to the element.
-                    this->Dragging = true;
-                }
-            }
+                    // Diff Width & Height. Not prediction anymore, recycled.
+                    PredictionW = this->PreviousW + (this->PreviousX - this->Rect.X);
+                    PredictionH = this->PreviousH + (this->PreviousY - this->Rect.Y);
 
-            if (this->ResizableDockFlags != -1 && this->Resizing == 0 && !this->Dragging) {
-                int CollidingDock = EKG::PointCollideDock(this->ResizableDockFlags, FX, FY, 0, this->ResizeOffset, this->Rect);
+                    W = PredictionW < (float) this->MinimumWidth ? (float) this->MinimumWidth : PredictionW;
+                    H = PredictionH < (float) this->MinimumHeight ? (float) this->MinimumHeight : PredictionH;
 
-                if (CollidingDock != -1) {
-                    if (CollidingDock == EKG::Dock::LEFT || CollidingDock == EKG::Dock::TOP) {
-                        // Its works like a drag.
-                        this->DragW = FX - (this->Rect.X);
-                        this->DragH = FY - (this->Rect.Y);
+                    this->Rect.W = W;
+                    this->Rect.H = H;
+                } else {
+                    X = this->Rect.X;
+                    Y = this->Rect.Y;
 
-                        // Save to use after.
-                        this->PreviousX = this->Rect.X;
-                        this->PreviousY = this->Rect.Y;
-                        this->PreviousW = this->Rect.W;
-                        this->PreviousH = this->Rect.H;
-                    } else {
-                        this->DragW = FX - this->Rect.W;
-                        this->DragH = FY - this->Rect.H;
+                    W = FX - this->DragW;
+                    H = FY - this->DragH;
+
+                    if (this->GetMasterId() != 0) {
+                        if (X + W > this->ScaledX + this->ScaledWidth) {
+                            W = this->ScaledX + this->ScaledWidth - X;
+                        }
+
+                        if (Y + H > this->ScaledY + this->ScaledHeight) {
+                            H = this->ScaledY + this->ScaledHeight - Y;
+                        }
+
+                        this->SyncPos();
                     }
 
-                    // Say true for resizing to the element.
-                    this->Resizing = CollidingDock;
+                    this->Rect.W = W < (float) this->MinimumWidth ? (float) this->MinimumWidth : W;
+                    this->Rect.H = H < (float) this->MinimumHeight ? (float) this->MinimumHeight : H;
                 }
+
+                MovingEvent = true;
             }
+
+            if (MovingEvent && this->IsMaster()) {
+                this->SyncSize();
+                EKG_CORE->Sync(this->Rect.X, this->Rect.Y, this->Rect.W, this->Rect.H, this->Children);
+            }
+
+            break;
         }
-    }
 
-    if (Event.type == SDL_FINGERUP) {
-        this->Hovered = false;
-        this->Dragging = false;
-        this->Resizing = 0;
+        case SDL_FINGERDOWN: {
+            float FX = Event.tfinger.x;
+            float FY = Event.tfinger.y;
+
+            EKG::ScaledFingerPos(FX, FY);
+
+            if (this->Hovered) {
+                if (this->DraggableDockFlags != 0 && !this->Dragging && this->Resizing == 0) {
+                    int CollidingDock = EKG::PointCollideDock(this->DraggableDockFlags, FX, FY, 0, this->DragOffset, this->Rect);
+
+                    if (CollidingDock != -1) {
+                        if (this->GetMasterId() == 0) {
+                            this->DragX = FX - this->Rect.X;
+                            this->DragY = FY - this->Rect.Y;
+                        } else {
+                            this->DragX = FX - (this->Rect.X - this->ScaledX);
+                            this->DragY = FY - (this->Rect.Y - this->ScaledY);
+                        }
+
+                        // Say true for dragging to the element.
+                        this->Dragging = true;
+                    }
+                }
+
+                if (this->ResizableDockFlags != -1 && this->Resizing == 0 && !this->Dragging) {
+                    int CollidingDock = EKG::PointCollideDock(this->ResizableDockFlags, FX, FY, 0, this->ResizeOffset, this->Rect);
+
+                    if (CollidingDock != -1) {
+                        if (CollidingDock == EKG::Dock::LEFT || CollidingDock == EKG::Dock::TOP) {
+                            // Its works like a drag.
+                            this->DragW = FX - (this->Rect.X);
+                            this->DragH = FY - (this->Rect.Y);
+
+                            // Save to use after.
+                            this->PreviousX = this->Rect.X;
+                            this->PreviousY = this->Rect.Y;
+                            this->PreviousW = this->Rect.W;
+                            this->PreviousH = this->Rect.H;
+                        } else {
+                            this->DragW = FX - this->Rect.W;
+                            this->DragH = FY - this->Rect.H;
+                        }
+
+                        // Say true for resizing to the element.
+                        this->Resizing = CollidingDock;
+                    }
+                }
+            }
+
+            break;
+        }
+
+        case SDL_FINGERUP: {
+            this->Hovered = false;
+            this->Dragging = false;
+            this->Resizing = 0;
+
+            break;
+        }
     }
 }
 
@@ -386,24 +394,34 @@ void EKG_Button::OnPreEvent(SDL_Event Event) {
 void EKG_Button::OnEvent(SDL_Event Event) {
     EKG_AbstractElement::OnEvent(Event);
 
-    if (Event.type == SDL_FINGERDOWN && this->Hovered) {
-        this->Pressed = true;
-    }
+    switch (Event.type) {
+        case SDL_FINGERDOWN: {
+            if (this->Hovered) {
+                this->Pressed = true;
+            }
 
-    if (Event.type == SDL_FINGERUP && this->Pressed) {
-        float FX = Event.tfinger.x;
-        float FY = Event.tfinger.y;
-
-        EKG::ScaledFingerPos(FX, FY);
-
-        this->HoveredBox = this->DetectPointCollideBox(FX, FY);
-        this->Hovered = this->IsFingerOver(FX, FY);
-
-        if (this->HoveredBox || this->Hovered) {
-            this->Checked = this->IsCheckBox() ? !this->Checked : this->Checked;
+            break;
         }
 
-        this->Pressed = false;
+        case SDL_FINGERUP: {
+            if (this->Pressed) {
+                float FX = Event.tfinger.x;
+                float FY = Event.tfinger.y;
+
+                EKG::ScaledFingerPos(FX, FY);
+
+                this->HoveredBox = this->DetectPointCollideBox(FX, FY);
+                this->Hovered = this->IsFingerOver(FX, FY);
+
+                if (this->HoveredBox || this->Hovered) {
+                    this->Checked = this->IsCheckBox() ? !this->Checked : this->Checked;
+                }
+
+                this->Pressed = false;
+            }
+
+            break;
+        }
     }
 }
 
